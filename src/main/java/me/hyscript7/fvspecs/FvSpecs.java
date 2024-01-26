2300a51c8bab0454cb5aa61e5aea1b3b5c94e389
HyScript7 <hyscript7@seznam.cz>
2024-01-26 21:37:55 +0100

    ✨ Enable for traveling to and from the Void Realm This will need refactoring later, as it's unreadable.

--- src/main/java/me/hyscript7/fvspecs/FvSpecs.java
+++ src/main/java/me/hyscript7/fvspecs/FvSpecs.java
@@ -1,5 +1,6 @@
 package me.hyscript7.fvspecs;
 
+import me.hyscript7.fvspecs.listeners.VoidRealmPortal;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class FvSpecs extends JavaPlugin {
@@ -7,6 +8,7 @@ public final class FvSpecs extends JavaPlugin {
     @Override
     public void onEnable() {
         // Plugin startup logic
+        this.getServer().getPluginManager().registerEvents(new VoidRealmPortal(this), this);
     }
 
     @Override
