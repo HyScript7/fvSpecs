fc856a07cf2ef3e8dadabb2d0f0eb9fc4bb8ea4b
HyScript7 <hyscript7@seznam.cz>
2024-01-29 22:14:05 +0100

    🐛Fix AntiBuild behaviour
    Not sure why, but it works now (for the most part)

--- src/main/java/me/hyscript7/fvspecs/listeners/voidrealm/behaviour/AntiBuild.java
+++ src/main/java/me/hyscript7/fvspecs/listeners/voidrealm/behaviour/AntiBuild.java
@@ -3,6 +3,7 @@
  */
 package me.hyscript7.fvspecs.listeners.voidrealm.behaviour;
 
+import org.bukkit.Material;
 import org.bukkit.block.Block;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
@@ -10,6 +11,7 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.event.block.BlockPlaceEvent;
 import org.bukkit.plugin.Plugin;
+import org.bukkit.potion.PotionEffectType;
 
 import java.util.Arrays;
 import java.util.logging.Logger;
@@ -18,24 +20,38 @@ public class AntiBuild implements Listener {
     private final Plugin plugin;
     private final Logger logger;
     private final String dimensionName = "world_fvspecs_voidrealm";
-    private final String[] allowedBlocks = {
-            "BEETROOTS",
-            "CACTUS",
-            "CARROTS",
-            "COCOA",
-            "CHORUS_FLOWER",
-            "KELP",
-            "MELON_STEM",
-            "NETHER_WART",
-            "POTATOES",
-            "PUMPKIN_STEM",
-            "SUGAR_CANE",
-            "SWEET_BERRY_BUSH",
-            "TORCHFLOWER",
-            "TORCHFLOWER_CROP",
-            "WHEAT",
-            "MELON",
-            "PUMPKIN"
+    private final Material[] whitelistedBlocks = {
+            Material.BEETROOT,
+            Material.BEETROOT_SEEDS,
+            Material.BEETROOTS,
+            Material.CACTUS,
+            Material.CARROT,
+            Material.CARROTS,
+            Material.COCOA,
+            Material.COCOA_BEANS,
+            Material.CHORUS_FLOWER,
+            Material.CHORUS_PLANT,
+            Material.KELP,
+            Material.KELP_PLANT,
+            Material.MELON,
+            Material.MELON_SEEDS,
+            Material.MELON_STEM,
+            Material.PUMPKIN,
+            Material.PUMPKIN_SEEDS,
+            Material.PUMPKIN_STEM,
+            Material.NETHER_WART,
+            Material.POTATO,
+            Material.POTATOES,
+            Material.SUGAR_CANE,
+            Material.SWEET_BERRIES,
+            Material.SWEET_BERRY_BUSH,
+            Material.WHEAT,
+            Material.WHEAT_SEEDS,
+            Material.CARROT,
+            Material.CARROTS,
+            Material.POTATO,
+            Material.POTATOES,
+            Material.BAMBOO,
     };
 
     public AntiBuild(Plugin p) {
@@ -46,14 +62,8 @@ public class AntiBuild implements Listener {
     @EventHandler
     public void onBlockBreakInVoid(BlockBreakEvent event) {
         Block block = event.getBlock();
-
-        // check if the event is happening in the right dimension
         if (block.getLocation().getWorld().getName().equals(dimensionName)) {
-
-            // check if the effected block is an exception and player is a void bound
-            if (!Arrays.asList(allowedBlocks).contains((block.getType().toString())) && playerIsNotVoidBoundRace(event.getPlayer())) {
-
-                // cancel if non exception
+            if (!(materialIsWhitelisted(block) && playerIsVoidBound(event.getPlayer()))) {
                 event.setCancelled(true);
             }
         }
@@ -62,20 +72,18 @@ public class AntiBuild implements Listener {
     @EventHandler
     public void onBlockPlaceInVoid(BlockPlaceEvent event) {
         Block block = event.getBlock();
-
-        // check if the event is happening in the right dimension
         if (block.getLocation().getWorld().getName().equals(dimensionName)) {
-
-            // check if the effected block is an exception
-            if (!Arrays.asList(allowedBlocks).contains((block.getType().toString())) && playerIsNotVoidBoundRace(event.getPlayer())) {
-
-                // cancel if non exception
+            if (!((materialIsWhitelisted(block) || block.getType().equals(Material.FARMLAND)) && playerIsVoidBound(event.getPlayer()))) {
                 event.setCancelled(true);
             }
         }
     }
 
-    private boolean playerIsNotVoidBoundRace(Player player) {
-        return true;
+    private boolean materialIsWhitelisted(Block b) {
+        return Arrays.asList(whitelistedBlocks).contains(b.getType());
+    }
+
+    private boolean playerIsVoidBound(Player player) {
+        return true; // TODO: Do an actuall race check here, instead of assuming yes. (Default should be false)
     }
 }
