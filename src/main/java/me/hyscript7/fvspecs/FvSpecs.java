170708ea705b7926ca08502ee5f05e345dbefef6
HyScript7 <hyscript7@seznam.cz>
2024-01-29 19:55:45 +0100

    🚩 Start Datastore on plugin init
    Effectively enables Database

--- src/main/java/me/hyscript7/fvspecs/FvSpecs.java
+++ src/main/java/me/hyscript7/fvspecs/FvSpecs.java
@@ -1,18 +1,28 @@
 package me.hyscript7.fvspecs;
 
+import me.hyscript7.fvspecs.datastore.DatastoreManager;
 import me.hyscript7.fvspecs.listeners.VoidRealmPortal;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.util.logging.Logger;
+
 public final class FvSpecs extends JavaPlugin {
+    private Logger logger = this.getLogger();
+    private DatastoreManager datastoreManager;
 
     @Override
     public void onEnable() {
         // Plugin startup logic
+        logger.info("Loading datastore manager");
+        this.datastoreManager = new DatastoreManager(this);
+        logger.info("Registering listeners");
         this.getServer().getPluginManager().registerEvents(new VoidRealmPortal(this), this);
     }
 
     @Override
     public void onDisable() {
         // Plugin shutdown logic
+        logger.info("Closing database file through datastore manager");
+        datastoreManager.getFileManager().close();
     }
 }
