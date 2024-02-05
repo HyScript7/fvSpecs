de57df01151c27183d032dfb35c525a5ef11f186
HyScript7 <hyscript7@seznam.cz>
2024-02-05 11:01:22 +0100

    🚑️ Remove datastoreManager mentions from fvSpecs main

--- src/main/java/me/hyscript7/fvspecs/FvSpecs.java
+++ src/main/java/me/hyscript7/fvspecs/FvSpecs.java
@@ -10,14 +10,10 @@ import java.util.logging.Logger;
 
 public final class FvSpecs extends JavaPlugin {
     private Logger logger = this.getLogger();
-    private DatastoreManager datastoreManager;
 
     @Override
     public void onEnable() {
         // Plugin startup logic
-        logger.info("Loading datastore manager");
-        this.datastoreManager = new DatastoreManager(this);
-        logger.info("Registering listeners");
         this.getServer().getPluginManager().registerEvents(new EntryListener(this), this);
         this.getServer().getPluginManager().registerEvents(new ExitListener(this), this);
         this.getServer().getPluginManager().registerEvents(new AntiBuild(this), this);
@@ -26,7 +22,5 @@ public final class FvSpecs extends JavaPlugin {
     @Override
     public void onDisable() {
         // Plugin shutdown logic
-        logger.info("Closing database file through datastore manager");
-        datastoreManager.getFileManager().close();
     }
 }
