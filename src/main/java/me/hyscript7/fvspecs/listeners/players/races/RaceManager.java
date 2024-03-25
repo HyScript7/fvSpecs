55452acb12bb348a8349f06acba88a65f22be23b
HyScript7 <hyscript7@seznam.cz>
2024-03-25 21:29:39 +0100

    Allow getting a race using an ID

--- src/main/java/me/hyscript7/fvspecs/listeners/players/races/RaceManager.java
+++ src/main/java/me/hyscript7/fvspecs/listeners/players/races/RaceManager.java
@@ -4,6 +4,8 @@ import me.hyscript7.fvspecs.datastore.DatastoreManager;
 import org.bukkit.event.Listener;
 import org.bukkit.plugin.Plugin;
 
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.logging.Logger;
 
 public class RaceManager implements Listener {
@@ -11,6 +13,7 @@ public class RaceManager implements Listener {
     private final Logger logger;
     private DatastoreManager datastoreManager;
     private Race[] races;
+    private Map<Integer, Race> raceLookupMap = new ConcurrentHashMap<>();
 
     public RaceManager(Plugin p, DatastoreManager d) {
         this.plugin = p;
@@ -20,10 +23,15 @@ public class RaceManager implements Listener {
         for (Race r : races) {
             this.plugin.getServer().getPluginManager().registerEvents((Listener) r, this.plugin);
             logger.info("Registered race " + r.getName());
+            raceLookupMap.put(r.getId(), r);
         }
     }
     public int getRandomRaceId() {
         int i = (int) (Math.random() * races.length);
         return races[i].getId();
+    }
+
+    public Race getRace(int id) {
+        return raceLookupMap.get(id);
     }
 }
