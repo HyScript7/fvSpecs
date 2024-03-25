cda23e2bd27b4e5cea54320002c37afa7f5eaaf4
HyScript7 <hyscript7@seznam.cz>
2024-03-25 21:19:45 +0100

    Add a function to check player's race against self

--- src/main/java/me/hyscript7/fvspecs/listeners/players/races/Race.java
+++ src/main/java/me/hyscript7/fvspecs/listeners/players/races/Race.java
@@ -1,5 +1,7 @@
 package me.hyscript7.fvspecs.listeners.players.races;
 
+import me.hyscript7.fvspecs.datastore.PlayerStore;
+
 public interface Race {
     final int ID = 0;
     final String NAME = "Human";
@@ -8,5 +10,8 @@ public interface Race {
     }
     default String getName() {
         return NAME;
+    }
+    default boolean playerRaceMatches(PlayerStore playerStore) {
+        return playerStore.getRace() == getId();
     }
 }
